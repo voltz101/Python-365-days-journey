35259c5c74d670ef3c7799210e8fb037ecf26b33
shadow <subedisuman66@gmail.com>
2026-06-20 23:24:28 +0545

    Add Day 1 Python basics and exercises

---
+++ day1.py
@@ -0,0 +1,40 @@
+print("Hello Python world!")
+
+# Introducing Variables here :
+
+message="Hello Python Crash Course World!"
+print(message)  
+
+greeting_message="Hi! I am Suman. Good to see you here in my first python code."
+print(greeting_message)
+
+# Avoiding Name errors in code Practice
+
+message="Good morning everyone!"
+print(message)
+
+#exercise-1
+
+Var_1="I am learnig Python."
+var_2="I am enjoying it."
+print(Var_1)
+print(var_2)
+
+#exercise-2
+
+var_1="I am learnig Python."
+var_2="I am enjoying it."
+var_1="I am learning Python and loving it."
+print(var_1)
+print(var_2)
+
+#practice code 1
+print("Welcome to Python Crash Course World!")
+print("Name:Suman Subedi") 
+print("Age:23") 
+print("Location:Kathmandu") 
+print("Country:Nepal")
+
+# My mini Project 1 :(my introduction)
+
+print("Welcome Everyone to my first ever code."+"My name is Suman Subedi."+"I am 23 years old."+"I live in Kathmandu, Nepal."+"I am a student of BBA."+" I am learning Python programming language."+"I am enjoying it a lot."+"I am very happy to be here in this course."+" I am very excited to learn more about Python programming language."+"I am very thankful to the author of this book for providing me this opportunity to learn Python programming language."+"I am very happy to be here in this course."+"I am very excited to learn more about Python programming language."+"I am very thankful to the author of this book for providing me this opportunity to learn Python programming language."+"I will become a professional python programmer by the end of the course. ")
